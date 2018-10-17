d960eaef2d2ec18f0675f5619832153111f273de
Svetoslav Ishev <svetoslavishev@abv.bg>
2018-10-17 17:11:58 +0300

    repositories changes

--- web/src/main/java/com/landlordcommunication/web/repositories/SqlResidenceRepository.java
+++ web/src/main/java/com/landlordcommunication/web/repositories/SqlResidenceRepository.java
@@ -1,37 +1,16 @@
 package com.landlordcommunication.web.repositories;
 
 import com.landlordcommunication.web.models.Residence;
-import com.landlordcommunication.web.repositories.ResidenceRepository;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class SqlResidenceRepository implements ResidenceRepository {
 
-    @Autowired
-    private SessionFactory sessionFactory;
 
 
     @Override
-    public Residence getResidenceByLandlord(int landlordId) {
-        Residence residence;
-
-        try (
-                Session session = sessionFactory.openSession();
-        ) {
-            session.beginTransaction();
-            residence = session.createQuery("from Residence where landlordId = :LandlordId")
-                    .setParameter("LandlordId", landlordId).
-
-          //  session.getTransaction().commit();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return residence;
-
+    public Residence getResidenceByLandlord() {
+        return null;
     }
 
     @Override
--- web/src/main/java/com/landlordcommunication/web/services/ResidenceServiceImpl.java
+++ web/src/main/java/com/landlordcommunication/web/services/ResidenceServiceImpl.java
@@ -1,10 +1,20 @@
 package com.landlordcommunication.web.services;
 
+import com.landlordcommunication.web.repositories.ResidenceRepository;
 import com.landlordcommunication.web.models.Residence;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class ResidenceServiceImpl implements ResidenceService {
+
+    private ResidenceRepository repository;
+
+    @Autowired
+    public ResidenceServiceImpl(ResidenceRepository repository)
+    {
+        this.repository = repository;
+    }
 
     @Override
     public Residence getResidenceByLandlord() {
