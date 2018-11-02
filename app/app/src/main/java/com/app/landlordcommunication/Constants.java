3357c57d7373465558031448e59874f8b160395d
Svetoslav Ishev <svetoslavishev@abv.bg>
2018-11-02 12:55:11 +0200

    refactored Constants class

--- app/app/src/main/java/com/app/landlordcommunication/Constants.java
+++ app/app/src/main/java/com/app/landlordcommunication/Constants.java
@@ -1,10 +1,20 @@
 package com.app.landlordcommunication;
 
 public class Constants {
+
+    // Slavi
+    public static final int USERS_LIST_IDENTIFIER = 7;
+    public static final String USERS_EXTRA_STRING = "userName";
+
+    // Valio
+    public static final int TEST_USER_ID = 14;
+    public static  int TEST_RESIDENCE_ID = 21;
+
+    // ALL SERVER URL'S
+
     //    public static final String BASE_SERVER_URL
     //       = "http://192.168.1.2:8080/api";
 
-    //TODO clarify server url string in here and in repository classes
 //    public static final String BASE_SERVER_URL
 //            = "http://10.54.227.80:8080/api/residences";
 
@@ -14,19 +24,6 @@ public class Constants {
 //    public static final String BASE_SERVER_URL
 //            = "http://192.168.0.101:8080/api";
 
-    public static final int TEST_USER_ID = 14;
-    public static  int TEST_RESIDENCE_ID = 21;
-            = "http://192.168.0.102:8080/api/users";
-    public static final int TEST_USER_ID = 18;
-
-    public static final int USERS_LIST_IDENTIFIER = 7;
-
-    public static final String USERS_EXTRA_STRING = "userName";
-
-    public static final int USERS_LIST_IDENTIFIER = 7;
-
-    public static final String USERS_EXTRA_STRING = "userName";
-
-    // TO BE CHANGED UPON APPLICATION LAUNCH
-
+            //= "http://192.168.0.102:8080/api/users";
+    //public static final int TEST_USER_ID = 18;
 }
