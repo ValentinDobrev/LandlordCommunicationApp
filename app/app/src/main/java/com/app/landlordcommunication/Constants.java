f29d6ed024d794dea3d82bc036f7fab6bf987c72
DOBREV <dobrev8444@gmail.com>
2018-11-07 11:42:32 +0200

    Merge branch 'master' of C:\Users\DOBREV\Desktop\finalProject with conflicts.

--- app/app/src/main/java/com/app/landlordcommunication/Constants.java
+++ app/app/src/main/java/com/app/landlordcommunication/Constants.java
@@ -3,19 +3,21 @@ package com.app.landlordcommunication;
 public class Constants {
 
     // Slavi
-    public static final String USER_RATING_EXTRA = "rating";
     public static final int USERS_LIST_IDENTIFIER = 7;
     public static final int USER_DETAILS_IDENTIFIER = 77;
     public static final String USERS_EXTRA_STRING = "user";
-    public static final String BASE_SERVER_URL = "http://192.168.43.238:8080/api";
+    //public static final String BASE_SERVER_URL = "http://192.168.43.15:8080/api";
 
     // Valio
+    public static final int TEST_USER_ID = 13;
     public static final int TEST_MESSAGE_RECEIVER_ID = 23;
-    public static int TEST_RESIDENCE_ID = 21;
+    public static  int TEST_RESIDENCE_ID = 13;
+    //public static final int TEST_USER_ID = 14;
+    //public static int TEST_RESIDENCE_ID = 21;
 
     //This is to keep track of which user is logged in:
 
-    public static int CURRENT_USER_ID = 14;
+    public static int CURRENT_USER_ID = -1;
 
     // ALL SERVER URL'S
 
@@ -25,8 +27,8 @@ public class Constants {
 //    public static final String BASE_SERVER_URL
 //            = "http://10.54.227.80:8080/api/residences";
 
-//    public static final String BASE_SERVER_URL
-//            = "http://10.137.45.134:8080/api";
+    public static final String BASE_SERVER_URL
+            = "http://10.137.45.134:8080/api";
 
 //    public static final String BASE_SERVER_URL
 //            = "http://192.168.0.101:8080/api";
