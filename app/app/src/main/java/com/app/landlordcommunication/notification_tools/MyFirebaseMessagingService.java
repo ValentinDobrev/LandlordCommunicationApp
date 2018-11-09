5f79d7f1fb927acca73384d4245a20906596b326
NikolaBunis <bunisbg@gmail.com>
2018-11-09 12:08:49 +0200

    Notifications set to display as a toast when app is open.

--- app/app/src/main/java/com/app/landlordcommunication/notification_tools/MyFirebaseMessagingService.java
+++ app/app/src/main/java/com/app/landlordcommunication/notification_tools/MyFirebaseMessagingService.java
@@ -1,32 +1,45 @@
 package com.app.landlordcommunication.notification_tools;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.app.landlordcommunication.R;
+import com.app.landlordcommunication.views.HomePage.HomePageActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-        private final String TAG = "JSA-FCM";
+    private final String TAG = "JSA-FCM";
 
-        @Override
-        public void onMessageReceived(RemoteMessage remoteMessage) {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
 
-            //TODO make sure we find a way to display received messages on the UI when app is turned on
+        //TODO think of a better way to display notifications in an open app than a long toast
 
-            //this part of the code logs the message in logcat
-            if (remoteMessage.getNotification() != null) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(() -> Toast.makeText(getApplicationContext(),
+                remoteMessage.getNotification().getTitle() + " " + remoteMessage.getNotification().getBody(),
+                Toast.LENGTH_LONG).show());
+
+        //the below logs the message in logcat - to be used for testing
+            /*if (remoteMessage.getNotification() != null) {
                 Log.e(TAG, "Title: " + remoteMessage.getNotification().getTitle());
                 Log.e(TAG, "Body: " + remoteMessage.getNotification().getBody());
             }
 
             if (remoteMessage.getData().size() > 0) {
                 Log.e(TAG, "Data: " + remoteMessage.getData());
-            }
-
-
-        }
-
-
+            }*/
+    }
 }
